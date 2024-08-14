99250a5d924320790a03f8a1db364f460abe7c99
amefure <shachi10969@gmail.com>
2024-08-14 21:02:32 +0900

    Initial Commit

---
+++ TestBeaconApp/TestBeaconAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestBeaconAppApp.swift
+//  TestBeaconApp
+//
+//  Created by t&a on 2024/08/14.
+//
+
+import SwiftUI
+
+@main
+struct TestBeaconAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
